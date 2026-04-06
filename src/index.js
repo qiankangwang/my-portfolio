7722237cccd556953ac19405f7e7ca31252dc8ec
qiankangwang <131919619+qiankangwang@users.noreply.github.com>
2026-04-06 00:55:02 -0700

    Update portfolio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
 import Portfolio from './Portfolio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
